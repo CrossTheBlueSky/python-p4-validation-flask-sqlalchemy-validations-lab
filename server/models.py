c21b0fafeee9ad363b5a12a55e674951aea4f7d6
Derek Mercedes <mercedes.derek@gmail.com>
2024-01-10 14:19:22 -0700

    tests passed more

--- server/models.py
+++ server/models.py
@@ -33,7 +33,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, unique=True, nullable=False)
     content = db.Column(db.String)
     category = db.Column(db.String)
     summary = db.Column(db.String)
